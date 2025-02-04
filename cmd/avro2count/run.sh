42db5ee54d0428e3fc4df3de7cf673e0890222f3
yanagitani <takanori.yanagitani@gmail.com>
2025-02-04 15:16:16 +0900

    docs: sample count

--- cmd/avro2count/run.sh
+++ cmd/avro2count/run.sh
@@ -3,3 +3,9 @@
 #cat sample.d/sample.json | jsons2maps2avro > ./sample.d/sample.avro
 
 cat sample.d/sample.avro | ./avro2count
+
+printf \
+	'%s\n' \
+	./sample.d/sample.avro \
+	./sample.d/sample.avro |
+	ENV_STDIN_AS_FILENAMES=true ./avro2count
